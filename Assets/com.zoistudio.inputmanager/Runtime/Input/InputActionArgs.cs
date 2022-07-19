e7106a08fed944ec064369b15ce9ffa60214e86d
Harris <harrissaleem1@hotmail.com>
2022-07-20 04:26:16 +1000

    compilation error

--- Assets/com.zoistudio.inputmanager/Runtime/Input/InputActionArgs.cs
+++ Assets/com.zoistudio.inputmanager/Runtime/Input/InputActionArgs.cs
@@ -4,17 +4,29 @@ using UnityEngine.Events;
 
 namespace ZoiStudio.InputManager
 {
-    public struct InputActionArgs
+    public struct InputActionArgs<T> where T : struct
     {
-        public GameAction Action;
+        public Enum Action;
+        public T InputData;
+    }
+
+    public struct TouchData
+    {
         public Vector2 LastTouchPosition;
         public float Velocity;
+    }
+
+    public struct DesktopData
+    {
+        public KeyCode KeyCode;
+        public Vector2 mousePosition;
+        public Vector2 deltaMouse;
     }
 
     [Serializable]
     public struct InputAction
     {
-        public GameAction Action;
+        public Enum Action;
         public UnityEvent OnAction;
     }
 }
--- Assets/com.zoistudio.inputmanager/Runtime/Input/InputHandler.cs
+++ Assets/com.zoistudio.inputmanager/Runtime/Input/InputHandler.cs
@@ -5,26 +5,26 @@ using UnityEngine;
 using ZoiStudio.InputManager;
 
 // This is just a sample. You should implement your own input handler class
-public class InputHandler : MonoBehaviour, IInputListener
+public class InputHandler : MonoBehaviour, IInputListener<TouchData>
 {
     public List<InputAction> gameActions;
 
     public virtual void OnEnable()
     {
-        InputEventManager.Subscribe(this, gameActions.Select(x => x.Action).ToArray());
-    }
-
-    public virtual void OnInput(InputActionArgs input)
-    {
-        var inputAction = gameActions.Where(x => x.Action == input.Action).First();
-        if (inputAction.OnAction != null)
-        {
-            inputAction.OnAction.Invoke();
-        }
+        InputEventManager<TouchData>.Subscribe(this, gameActions.Select(x => x.Action).ToArray());
     }
 
     public virtual void OnDisable()
     {
-        InputEventManager.UnSubscribe(this, gameActions.Select(x => x.Action).ToArray());
+        InputEventManager<TouchData>.UnSubscribe(this, gameActions.Select(x => x.Action).ToArray());
+    }
+
+    public void OnInput(InputActionArgs<TouchData> action)
+    {
+        var inputAction = gameActions.Where(x => x.Action == action.Action).First();
+        if (inputAction.OnAction != null)
+        {
+            inputAction.OnAction.Invoke();
+        }
     }
 }
